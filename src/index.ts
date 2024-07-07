76bb078166e27e384d3e1e44a5a6e5cbd351b214
telida <sprv.adilet@gmail.com>
2024-07-07 07:17:14 +0500

    no drag direct mode added

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import MapLibreDraw from "@telida/maplibre-gl-draw";
+
+export function helloWorld(): string {
+    return 'Hello, world!';
+}
+
+class NoDragDirectMode extends (MapLibreDraw.modes.direct_select as any) {
+    onDrag(state: any, e: any) {
+        console.log('CustomDirectSelect.onDrag', state, e);
+
+        if (state.canDragMove !== true) return;
+        state.dragMoving = true;
+        e.originalEvent.stopPropagation();
+
+        const delta = {
+            lng: e.lngLat.lng - state.dragMoveLocation.lng,
+            lat: e.lngLat.lat - state.dragMoveLocation.lat
+        };
+        if (state.selectedCoordPaths.length > 0) this.dragVertex(state, e, delta);
+        // else this.dragFeature(state, e, delta);
+
+        state.dragMoveLocation = e.lngLat;
+    }
+}
